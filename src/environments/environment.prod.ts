00d0332ef84ec6215af1ed8136d6b12a56c2a1f6
Rinat Madreiter <umfragmail@gmx.de>
2022-06-02 09:58:26 +0200

    added endscreen & firebase implementation with url to play online

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-22675',
+    appId: '1:853799688907:web:6cb7f876e7f171405a594e',
+    storageBucket: 'ring-of-fire-22675.appspot.com',
+    apiKey: 'AIzaSyD9gcXc5NBmX_D3XN6qV2f2hAacK7MsPpg',
+    authDomain: 'ring-of-fire-22675.firebaseapp.com',
+    messagingSenderId: '853799688907',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-22675',
+    appId: '1:853799688907:web:6cb7f876e7f171405a594e',
+    storageBucket: 'ring-of-fire-22675.appspot.com',
+    apiKey: 'AIzaSyD9gcXc5NBmX_D3XN6qV2f2hAacK7MsPpg',
+    authDomain: 'ring-of-fire-22675.firebaseapp.com',
+    messagingSenderId: '853799688907',
+  },
   production: false
 };
 
